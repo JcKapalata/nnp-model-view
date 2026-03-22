53d70486bc719528f6a221e949d3f88cc98db41a
JcKaps <kapalatajc@gmail.com>
2026-03-22 22:26:50 +0200

    ajout du composant navbar sans style

--- src/app/navbar.component/navbar.component.ts
+++ src/app/navbar.component/navbar.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'navbar-component',
-  imports: [],
+  imports: [
+    MatToolbarModule, 
+    MatButtonModule, 
+    MatIconModule, 
+    MatMenuModule
+  ],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss',
+  styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  isMenuOpen = signal(false);
 
+  toggleMenu() {
+    this.isMenuOpen.update(v => !v);
+  }
 }
