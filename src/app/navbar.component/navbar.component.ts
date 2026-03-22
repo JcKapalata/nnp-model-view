90c9b02ee262ba0353e2357a364cd03417bab03c
JcKaps <kapalatajc@gmail.com>
2026-03-22 22:47:33 +0200

    ajout du style du navbar

--- src/app/navbar.component/navbar.component.ts
+++ src/app/navbar.component/navbar.component.ts
@@ -3,10 +3,13 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'navbar-component',
+  standalone: true,
   imports: [
+    CommonModule,
     MatToolbarModule, 
     MatButtonModule, 
     MatIconModule, 
@@ -16,9 +19,24 @@ import { MatMenuModule } from '@angular/material/menu';
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  // État du menu mobile
   isMenuOpen = signal(false);
+
+  // État de la langue sélectionnée (Français par défaut)
+  currentLang = signal({
+    label: 'FR',
+    flag: 'lang/francais.png'
+  });
 
   toggleMenu() {
     this.isMenuOpen.update(v => !v);
+  }
+
+  changeLang(lang: 'fr' | 'en') {
+    if (lang === 'fr') {
+      this.currentLang.set({ label: 'FR', flag: 'lang/francais.png' });
+    } else {
+      this.currentLang.set({ label: 'EN', flag: 'lang/anglais.png' });
+    }
   }
 }
